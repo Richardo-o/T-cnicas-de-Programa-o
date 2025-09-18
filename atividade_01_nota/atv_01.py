49a14691de0c6a142d37ebf66f4ffaad12f94619
DSM3 <fatec-dsm3@academico.intra>
2025-09-18 19:04:29 -0300

    atualização

--- atividade_01_nota/atv_01.py
+++ atividade_01_nota/atv_01.py
@@ -4,6 +4,8 @@ tela = Tk()
 
 tela.geometry("700x600")
 
+tela.configure(background='#1e3743')
+
 lbl_nome = Label(tela, text = "Nome: ").place (x=220, y=0)
 lbl_idade = Label(tela, text= "Idade: ").place(x=220, y=30)
 lbl_tel = Label(tela, text="Telefone: ").place(x=200, y=58)
@@ -31,9 +33,22 @@ def meu_click():
     botao.pack()
     
     
-btn_botao = Button(tela, text="Salvar", command=meu_click)
+
+   
+    
+btn_botao = Button(tela, text="Cadastrar", command=meu_click)
 btn_botao.pack()
 
+lbl_p1 = Label(tela, text= "Nome: Zileide Mariano| Idade: 54 | Telefone: (13)99673-1592 | Grau parentesco: 3º")
+lbl_p1.pack()
 
+lbl_p2 = Label(tela, text= "Nome: Jailson Mariano | Idade: 55 | Telefone: (13)99623-3456 | Grau parentesco: 3º")
+lbl_p2.pack()
+
+lbl_p3 = Label(tela, text= "Nome: José Mariano | Idade: 54 | Telefone: (13)99640-3654 | Grau parentesco: 3º")
+lbl_p3.pack()
+
+lbl_p4 = Label(tela, text= "Nome: Maria Julia | Idade: 87 | Telefone: (13)99343-3633 | Grau parentesco: 2º")
+lbl_p4.pack()
 
 tela.mainloop()
